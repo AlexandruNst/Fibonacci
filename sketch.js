fe6969e4d97bb417b349422f73d3fe5ce3f05b91
Alexandru Nastase <axn778@cs.bham.ac.uk>
2018-01-08 14:23:57 +0000

    Checking change in email

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ let r = 0;
 let first = 1;
 let second = 1;
 let third = 2;
+let t;
+let prevX = 0;
+let prevY = 0;
 
 function setup() {
     angleMode(DEGREES);
@@ -13,10 +16,82 @@ function setup() {
 
 function draw() {
 
+    translate(width / 2, height / 2);
+
     first = second;
     second = third;
     third = first + second;
 
-    console.log(third);
+    t = map(third, 0, 1000000000000, 0, width);
+
+    console.log(t);
+
+    //noStroke();
+    fill(255);
+    ellipse(t, t, 10, 10);
+    line(prevX, prevY, t, t);
+    //rect(t, t, t, t);
+    prevX = t;
+    prevY = t;
+
+    ///////////////
+
+    first = second;
+    second = third;
+    third = first + second;
+
+    t = map(third, 0, 1000000000000, 0, width);
+
+    console.log(t);
+
+    //noStroke();
+    fill(255);
+    ellipse(t, -t, 10, 10);
+    line(prevX, prevY, t, -t);
+
+    prevX = t;
+    prevY = -t;
+
+    //rect(t, -t, t, t);
+    /////////////
+
+    first = second;
+    second = third;
+    third = first + second;
+
+    t = map(third, 0, 1000000000000, 0, width);
+
+    console.log(t);
+
+    //noStroke();
+    fill(255);
+    ellipse(-t, -t, 10, 10);
+    line(prevX, prevY, -t, -t);
+
+    prevX = -t;
+    prevY = -t;
+    //rect(-t, -t, t, t);
+
+    /////////////////////////////
+
+    first = second;
+    second = third;
+    third = first + second;
+
+    t = map(third, 0, 1000000000000, 0, width);
+
+    console.log(t);
+
+    //noStroke();
+    fill(255);
+    ellipse(-t, t, 10, 10);
+    noFill();
+    arc(0, 0, t, t, PI, (3.0 * PI) / 2)
+    line(prevX, prevY, -t, t);
+    //rect(-t, t, t, t);
+
+    if (t > 1000000000000) {
+        noLoop();
+    }
 
 }
